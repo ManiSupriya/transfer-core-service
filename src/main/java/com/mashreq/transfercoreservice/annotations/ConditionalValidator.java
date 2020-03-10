be4a32128e7e5d90a2a160fd19e02ca7a01e97fc
designer61 <designer61@designer61s-MacBook-Pro.local>
2020-03-10 19:00:42 +0400

    Annotations and Fund Transfer Req DTO

---
+++ src/main/java/com/mashreq/transfercoreservice/annotations/ConditionalValidator.java
@@ -0,0 +1,84 @@
+package com.mashreq.transfercoreservice.annotations;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Base64;
+import java.util.stream.Stream;
+
+import static org.apache.commons.beanutils.BeanUtils.getProperty;
+
+/**
+ * @author shahbazkh
+ * @date 3/4/20
+ */
+public class ConditionalValidator implements ConstraintValidator<ConditionalRequired, Object> {
+
+    private static final int CHECK_DIGIT_LENGTH = 4;
+    private String fieldName;
+    private String dependentFieldName;
+    private String[] anyMatch;
+    private String[] nonMatch;
+    private int size;
+    private boolean decodeBase64;
+
+    @Override
+    public void initialize(ConditionalRequired constraintAnnotation) {
+        fieldName = constraintAnnotation.fieldName();
+        dependentFieldName = constraintAnnotation.dependentFieldName();
+        anyMatch = constraintAnnotation.anyMatch();
+        nonMatch = constraintAnnotation.noneMatch();
+        size = constraintAnnotation.size();
+        decodeBase64 = constraintAnnotation.decode();
+
+    }
+
+    @Override
+    public boolean isValid(Object object, ConstraintValidatorContext constraintValidatorContext) {
+        try {
+            final String dependentFieldValue = getDependentFieldValue(dependentFieldName, object);
+            if (null != anyMatch && anyMatch.length > 0) {
+                if (Stream.of(anyMatch).anyMatch(x -> x.equals(dependentFieldValue))) {
+                    final String fieldValue = getFieldValue(fieldName, decodeBase64, object);
+                    if (size != -1 && fieldValue.length() != size)
+                        return false;
+                }
+            }
+
+            if (null != nonMatch && nonMatch.length > 0) {
+                if (Stream.of(nonMatch).noneMatch(x -> x.equals(dependentFieldValue))) {
+                    final String fieldValue = getFieldValue(fieldName, decodeBase64, object);
+                    if (size != -1 && fieldValue.length() != size) {
+                        return false;
+                    } else {
+                        return fieldValue.length() > 0;
+                    }
+
+                }
+
+            }
+
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+
+    private String getFieldValue(final String fieldName, final boolean decodeBase64, final Object object) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        final String fieldValue = StringUtils.trim(getProperty(object, fieldName));
+
+
+        return StringUtils.isBlank(fieldValue)
+                ? ""
+                : decodeBase64
+                ? new String(Base64.getDecoder().decode(fieldValue))
+                : fieldValue;
+
+    }
+
+    private String getDependentFieldValue(final String dependentFieldName, final Object object) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        return StringUtils.trim(getProperty(object, dependentFieldName));
+    }
+}
--- src/main/java/com/mashreq/transfercoreservice/api/FundTransferRequestDTO.java
+++ src/main/java/com/mashreq/transfercoreservice/api/FundTransferRequestDTO.java
@@ -1,8 +1,12 @@
 package com.mashreq.transfercoreservice.api;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.mashreq.transfercoreservice.annotations.Account;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 /**
@@ -11,13 +15,32 @@ import java.math.BigDecimal;
  */
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FundTransferRequestDTO {
+
+    @Account
     private String fromAccount;
+
+    @Account
     private String toAccount;
+
+    @NotBlank
+    @Positive
     private BigDecimal amount;
+
+    @NotBlank
+    private String serviceType;
+
+    @NotBlank
     private String currency;
+
+    @NotBlank
     private String purposeCode;
+
     private String dealNumber;
+
+    @NotBlank
     private String finTxnNo;
+
+    @NotBlank
+    private Long beneficiaryId;
 }
