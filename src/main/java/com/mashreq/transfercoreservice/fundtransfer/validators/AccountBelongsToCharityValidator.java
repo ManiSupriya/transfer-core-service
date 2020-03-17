ac777503fe2bcf5ac3262573184156482bdb10aa
designer61 <designer61@designer61s-MacBook-Pro.local>
2020-03-17 13:32:20 +0400

    Added code for validator

--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/validators/AccountBelongsToCharityValidator.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/validators/AccountBelongsToCharityValidator.java
@@ -3,11 +3,12 @@ package com.mashreq.transfercoreservice.fundtransfer.validators;
 import com.mashreq.transfercoreservice.fundtransfer.dto.FundTransferMetadata;
 import com.mashreq.transfercoreservice.fundtransfer.dto.FundTransferRequestDTO;
 
-import java.util.Map;
-
 /**
  * @author shahbazkh
  */
-public interface Validator {
-    ValidationResult validate(FundTransferRequestDTO request, FundTransferMetadata metadata, ValidationContext context );
+public class AccountBelongsToCharityValidator implements Validator{
+    @Override
+    public ValidationResult validate(FundTransferRequestDTO request, FundTransferMetadata metadata, ValidationContext context) {
+        return null;
+    }
 }
