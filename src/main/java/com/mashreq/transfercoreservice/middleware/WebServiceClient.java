5d9202ef9c2ba1d44e686bc4ad338cbbfa4253b1
designer61 <shahbazkh@mashreq.com>
2020-03-30 14:16:25 +0400

    Added code for timeouts and elapse time

--- src/main/java/com/mashreq/transfercoreservice/middleware/WebServiceClient.java
+++ src/main/java/com/mashreq/transfercoreservice/middleware/WebServiceClient.java
@@ -1,10 +1,17 @@
 package com.mashreq.transfercoreservice.middleware;
 
+import com.mashreq.ms.exceptions.GenericExceptionHandler;
+import com.mashreq.transfercoreservice.errors.TransferErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.ws.client.WebServiceIOException;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.transport.http.ClientHttpRequestMessageSender;
 
+import static com.mashreq.transfercoreservice.errors.TransferErrorCode.MW_CONNECTION_TIMEOUT;
 
+
+@Slf4j
 public class WebServiceClient extends WebServiceGatewaySupport {
 
     /**
@@ -20,7 +27,15 @@ public class WebServiceClient extends WebServiceGatewaySupport {
     }
 
     public Object exchange(Object requestPayload) {
-        return getWebServiceTemplate().marshalSendAndReceive(getDefaultUri(), requestPayload);
+
+        Object result = null;
+        try {
+            result = getWebServiceTemplate().marshalSendAndReceive(getDefaultUri(), requestPayload);
+        } catch (WebServiceIOException e) {
+            log.error("Cannot connect to middle ware {} ", e);
+            GenericExceptionHandler.handleError(MW_CONNECTION_TIMEOUT, MW_CONNECTION_TIMEOUT.getErrorMessage());
+        }
+        return result;
     }
 
 }
