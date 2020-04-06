934dabe206933ed75df0dbf1beb43233b903c28c
Deepa S <deepash@mashreq.com>
2020-04-06 22:20:57 +0400

    rebase done

--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/strategy/LocalFundTransferStrategy.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/strategy/LocalFundTransferStrategy.java
@@ -18,6 +18,7 @@ import com.mashreq.transfercoreservice.limits.LimitValidatorResultsDto;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
