f54d64e80b2f23d7c5551d49f1f225b877acb5c5
designer61 <shahbazkh@mashreq.com>
2020-04-16 11:22:00 +0400

    Beneficiary error handling with new error code

--- src/main/java/com/mashreq/transfercoreservice/client/ErrorUtils.java
+++ src/main/java/com/mashreq/transfercoreservice/client/ErrorUtils.java
@@ -4,33 +4,17 @@ import com.mashreq.transfercoreservice.config.feign.FeignResponse;
 import com.mashreq.webcore.dto.response.Response;
 import org.apache.commons.lang3.StringUtils;
 
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
-
 /**
  * @author shahbazkh
  * @date 4/1/20
  */
 public class ErrorUtils {
 
-    public static boolean hasError(FeignResponse feignResponse) {
-        return isNotBlank(feignResponse.getErrorId()) || isNotBlank(feignResponse.getErrorCode());
-    }
 
-//    public static boolean hasError(Response response) {
-//        return isNotBlank(response.getErrorId()) || isNotBlank(response.getErrorCode());
-//    }
-
-//    public static String getErrorDetails(Response response) {
-//        if (StringUtils.isNotBlank(response.getErrorDetails())) {
-//            return response.getErrorCode() + "," + response.getErrorDetails();
-//        }
-//        return response.getErrorCode();
-//    }
-
-    public static String getErrorDetails(FeignResponse feignResponse) {
-        if (StringUtils.isNotBlank(feignResponse.getErrorDetails())) {
-            return feignResponse.getErrorCode() + "," + feignResponse.getErrorDetails();
+    public static String getErrorDetails(Response response) {
+        if (StringUtils.isNotBlank(response.getErrorDetails())) {
+            return response.getErrorCode() + "," + response.getErrorDetails();
         }
-        return feignResponse.getErrorCode();
+        return response.getErrorCode();
     }
 }
