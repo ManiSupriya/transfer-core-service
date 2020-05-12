cce2cee7c0c906f95ddb3c629f67432632077783
Deepa S <deepash@mashreq.com>
2020-05-12 18:37:02 +0400

    removed extra delimiter

--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/strategy/utils/CustomerDetailsUtils.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/strategy/utils/CustomerDetailsUtils.java
@@ -4,12 +4,15 @@ package com.mashreq.transfercoreservice.fundtransfer.strategy.utils;
 import com.mashreq.transfercoreservice.client.dto.BeneficiaryDto;
 import com.mashreq.transfercoreservice.client.mobcommon.dto.AddressTypeDto;
 import com.mashreq.transfercoreservice.client.mobcommon.dto.CustomerDetailsDto;
+import com.mashreq.transfercoreservice.client.mobcommon.dto.CustomerPhones;
 import com.mashreq.transfercoreservice.fundtransfer.dto.QuickRemitFundTransferRequest;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author shahbazkh
@@ -39,8 +42,11 @@ public class CustomerDetailsUtils {
      * @return
      */
     public static String getMobileNumber(CustomerDetailsDto customerDetails) {
-        return customerDetails.getPhones().stream()
-                .filter(phone -> PHONE_NUMBER_TYPES.contains(phone.getPhoneNumberType()) && StringUtils.isNotBlank(phone.getMobNumber()))
+        final Map<String, CustomerPhones> phoneNumByType = customerDetails.getPhones().stream()
+                .collect(Collectors.toMap(CustomerPhones::getPhoneNumberType, phone -> phone));
+        return PHONE_NUMBER_TYPES.stream()
+                .filter(type -> phoneNumByType.containsKey(type) && StringUtils.isNotBlank(phoneNumByType.get(type).getMobNumber()))
+                .map(type -> phoneNumByType.get(type))
                 .findFirst()
                 .map(phone -> phone.getMobNumber()).orElse("");
     }
@@ -51,7 +57,8 @@ public class CustomerDetailsUtils {
      * @param customerDetails
      * @return
      */
-    public static QuickRemitFundTransferRequest deriveSenderIdNumberAndAddress(QuickRemitFundTransferRequest remitFundTransferRequest, CustomerDetailsDto customerDetails) {
+    public static QuickRemitFundTransferRequest deriveSenderIdNumberAndAddress(QuickRemitFundTransferRequest remitFundTransferRequest,
+                                                                               CustomerDetailsDto customerDetails) {
         String address = deriveAddress(customerDetails.getAddress());
             return remitFundTransferRequest.toBuilder().senderIDType(customerDetails.getUniqueIDName())
                     .senderIDNumber(customerDetails.getUniqueIDValue()).senderAddress(address).build();
@@ -63,13 +70,14 @@ public class CustomerDetailsUtils {
                 StringUtils.defaultIfBlank(addressTypeDto.getAddress2(), "") + DELIMTER +
                 StringUtils.defaultIfBlank(addressTypeDto.getAddress3(), "") + DELIMTER +
                 StringUtils.defaultIfBlank(addressTypeDto.getAddress4(), "") + DELIMTER +
-                StringUtils.defaultIfBlank(addressTypeDto.getAddress5(), "") + DELIMTER;
+                StringUtils.defaultIfBlank(addressTypeDto.getAddress5(), "");
     }
 
 
 
     public static String deriveAddress(List<AddressTypeDto> address) {
-        final Optional<AddressTypeDto> first = address.stream().filter(a -> ADDRESS_TYPES.contains(a.getAddressType())).findFirst();
+        final Map<String, AddressTypeDto> addressByType = address.stream().collect(Collectors.toMap(AddressTypeDto::getAddressType, addr -> addr));
+        final Optional<AddressTypeDto> first = ADDRESS_TYPES.stream().filter(type -> addressByType.containsKey(type)).map(key -> addressByType.get(key)).findFirst();
         return first.map(add -> generateSenderAddress(add)).orElse("");
     }
 
