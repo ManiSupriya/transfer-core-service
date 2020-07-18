55ce93e0e51240f0ec4efdc5c1f1e669dbfc0b34
suresh pasupuleti <sureshpas@mashreq.com>
2020-07-18 14:19:41 +0530

    disabling otp service

--- src/main/java/com/mashreq/transfercoreservice/cardlesscash/service/impl/CardLessCashServiceImpl.java
+++ src/main/java/com/mashreq/transfercoreservice/cardlesscash/service/impl/CardLessCashServiceImpl.java
@@ -86,14 +86,14 @@ public class CardLessCashServiceImpl implements CardLessCashService {
 		verifyOTPRequestDTO.setRedisKey(metaData.getUserCacheKey());
 		log.info("cardLessCash Generation otp request{} ", verifyOTPRequestDTO);
 		Response<VerifyOTPResponseDTO> verifyOTP = otpService.verifyOTP(verifyOTPRequestDTO);
-		log.info("cardLessCash Generation otp response{} ", verifyOTP);
+		/*log.info("cardLessCash Generation otp response{} ", verifyOTP);
 		if(!verifyOTP.getData().isAuthenticated()) {
 			asyncUserEventPublisher.publishFailedEsbEvent(FundTransferEventType.CARD_LESS_CASH_OTP_DOES_NOT_MATCH,
 					metaData, CARD_LESS_CASH, metaData.getChannelTraceId(),
 					TransferErrorCode.OTP_EXTERNAL_SERVICE_ERROR.toString(),
 					TransferErrorCode.OTP_EXTERNAL_SERVICE_ERROR.getErrorMessage(), TransferErrorCode.OTP_EXTERNAL_SERVICE_ERROR.getErrorMessage());
 			GenericExceptionHandler.handleError(TransferErrorCode.OTP_EXTERNAL_SERVICE_ERROR, verifyOTP.getErrorDetails(), verifyOTP.getErrorDetails());
-		}
+		}*/
 		try {
 			cardLessCashGenerationResponse = accountService
 					.cardLessCashRemitGenerationRequest(cardLessCashGenerationRequest, userMobileNumber);
