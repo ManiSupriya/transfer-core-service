d1e3509f1f9c6b1c751fe8707a1234a5f3008d43
suresh pasupuleti <sureshpas@mashreq.com>
2020-08-17 17:59:34 +0530

    update deal validation

--- src/main/java/com/mashreq/transfercoreservice/client/MaintenanceClient.java
+++ src/main/java/com/mashreq/transfercoreservice/client/MaintenanceClient.java
@@ -4,13 +4,21 @@ import com.mashreq.transfercoreservice.client.dto.CoreCurrencyConversionRequestD
 import com.mashreq.transfercoreservice.client.dto.CountryMasterDto;
 import com.mashreq.transfercoreservice.client.dto.CurrencyConversionDto;
 import com.mashreq.transfercoreservice.config.feign.FeignConfig;
+import com.mashreq.transfercoreservice.fundtransfer.dto.DealEnquiryDto;
 import com.mashreq.webcore.dto.response.Response;
+
+import io.swagger.annotations.ApiParam;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+
+import javax.validation.constraints.NotEmpty;
 
 /**
  * Feign Client
@@ -26,6 +34,9 @@ public interface MaintenanceClient {
     @GetMapping("/v2/api/countries")
     Response<List<CountryMasterDto>> getAllCountries(@RequestParam final String channel, @RequestParam final String region,
                                                      @RequestParam final Boolean active);
+    
+    @GetMapping("/api/deal-enquiry/{dealNumber}")
+    public ResponseEntity<Response<DealEnquiryDto>> getFXDealInformation( @ApiParam("FX Deal Number")  @NotEmpty @PathVariable("dealNumber") String dealNumber);
 
 
 
