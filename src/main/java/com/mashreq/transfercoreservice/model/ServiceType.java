93b2478a20de3c5f84dc5907cbc5f54d26f3aec4
PallaviG <PallaviG@mashreq.com>
2020-08-31 15:23:01 +0400

    fixed table object as per new structure

--- src/main/java/com/mashreq/transfercoreservice/model/ServiceType.java
+++ src/main/java/com/mashreq/transfercoreservice/model/ServiceType.java
@@ -3,9 +3,10 @@ package com.mashreq.transfercoreservice.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "service_type_ms")
@@ -15,28 +16,17 @@ public class ServiceType {
 	@Id
 	@Column(name = "code", length = 20)
 	private String code;
-
 	@Column(length = 100)
 	private String name;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "country_id")
-	private Country country;
-
-	private Integer suggestedPosition;
-
+	private String serviceGroup;
 	// configs
 	private Long coolingPeriodMin = 0L;
 	private String minAmount;
 	private String maxAmount;
+	private String multipleOf;
 	private Boolean fractialPayment;
 	private Boolean partialPayment;
 	private Boolean overPayment;
-	private Boolean advancePayment;
-
-	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "service_type_payment_ranges_ms",
-			joinColumns = @JoinColumn(columnDefinition = "DECIMAL(7,2)"))
-	private List<BigDecimal> paymentRange;
-
+	private Boolean availableBalance;
+	private Boolean dueAmount;
 }
