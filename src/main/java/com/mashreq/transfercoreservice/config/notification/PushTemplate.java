887a3f1f263e3717b405b7c1e53af0079ad658bb
suresh pasupuleti <sureshpas@mashreq.com>
2020-12-21 17:40:43 +0400

    updated push Notification

--- src/main/java/com/mashreq/transfercoreservice/config/notification/PushTemplate.java
+++ src/main/java/com/mashreq/transfercoreservice/config/notification/PushTemplate.java
@@ -1,5 +1,11 @@
 package com.mashreq.transfercoreservice.config.notification;
-
+/**
+ * BUG 36630 - Local & INFT - SMS - customer contact center number is not correct in SMS for fund transfer
+ * 
+ *  update: 
+ * this is fix for the wrong customer care details in SMS, Now SMS Info populating based on the Segment from segment_ms table
+ * 
+ */
 
 import com.mashreq.transfercoreservice.notification.model.CustomerNotification;
 import lombok.Data;
@@ -8,6 +14,8 @@ import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
 
+import static com.mashreq.transfercoreservice.notification.model.NotificationType.LOCAL_ACCOUNT_TRANSACTION;
+import static com.mashreq.transfercoreservice.notification.model.NotificationType.OTHER_ACCOUNT_TRANSACTION;
 import static com.mashreq.transfercoreservice.notification.model.NotificationType.OWN_ACCOUNT_TRANSACTION;
 
 
@@ -20,6 +28,12 @@ public class PushTemplate {
 
     public String getPushTemplate(String type, CustomerNotification customerNotification) {
         if(type.equals(OWN_ACCOUNT_TRANSACTION)){
+            return MessageFormat.format(ownAccountTransactionInitiated, customerNotification.getChannel(),customerNotification.getCurrency(),customerNotification.getAmount(),customerNotification.getTxnRef(),customerNotification.getSegment().getCustomerCareNumber());
+        }
+        if(type.equals(LOCAL_ACCOUNT_TRANSACTION)){
+            return MessageFormat.format(ownAccountTransactionInitiated, customerNotification.getChannel(),customerNotification.getCurrency(),customerNotification.getAmount(),customerNotification.getTxnRef(),customerNotification.getSegment().getCustomerCareNumber());
+        }
+        if(type.equals(OTHER_ACCOUNT_TRANSACTION)){
             return MessageFormat.format(ownAccountTransactionInitiated, customerNotification.getChannel(),customerNotification.getCurrency(),customerNotification.getAmount(),customerNotification.getTxnRef(),customerNotification.getSegment().getCustomerCareNumber());
         }
         return "";
