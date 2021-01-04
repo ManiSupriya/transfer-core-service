3be6edf53eb94038195cfb44d669cc16f95cd910
Ankush Singhal <ankushs@mashreq.com>
2021-01-04 15:22:03 +0400

    boolean cast fix

--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/dto/LimitValidatorResponse.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/dto/LimitValidatorResponse.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Builder
 public class LimitValidatorResponse {
 
-    private boolean isValid;
+    private Boolean isValid;
     private BigDecimal currentAvailableAmount;
     private Integer currentAvailableCount;
     private String amountRemark;
