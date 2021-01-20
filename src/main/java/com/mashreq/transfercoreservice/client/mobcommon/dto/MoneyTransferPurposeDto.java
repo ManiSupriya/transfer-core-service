81c0d18d35551bf2027b98ae8cb33398eb60ed60
suresh pasupuleti <sureshpas@mashreq.com>
2021-01-20 04:13:15 +0400

    updated DTO

--- src/main/java/com/mashreq/transfercoreservice/client/mobcommon/dto/MoneyTransferPurposeDto.java
+++ src/main/java/com/mashreq/transfercoreservice/client/mobcommon/dto/MoneyTransferPurposeDto.java
@@ -1,15 +1,34 @@
 package com.mashreq.transfercoreservice.client.mobcommon.dto;
 
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Data
+@Builder(toBuilder = true)
 @NoArgsConstructor
+@AllArgsConstructor
 public class MoneyTransferPurposeDto {
 
     private String purposeCode;
     private String purposeDesc;
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MoneyTransferPurposeDto that = (MoneyTransferPurposeDto) o;
+        return purposeCode.equals(that.purposeCode) &&
+                purposeDesc.equals(that.purposeDesc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(purposeCode, purposeDesc);
+    }
 
 }
