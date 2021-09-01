50c9497b0d1793cc7dd027140693f05377264b02
Fahad Iqbal Mughal <fahadmu@mashreq.com>
2021-09-01 12:07:07 +0400

    removel of notification status code

--- src/main/java/com/mashreq/transfercoreservice/repository/NotificationStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mashreq.transfercoreservice.repository;
-
-import com.mashreq.transfercoreservice.model.NotificationStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationStatusRepository extends JpaRepository<NotificationStatus, Long> {
-}
