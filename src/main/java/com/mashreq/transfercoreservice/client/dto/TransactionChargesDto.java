4a0f1d21499e67a3676dfe6f0ce5ab2ef160c190
Ankush Singhal <ankushs@mashreq.com>
2021-11-03 14:55:05 +0400

    bug fix for bank name footer

--- src/main/java/com/mashreq/transfercoreservice/client/dto/TransactionChargesDto.java
+++ src/main/java/com/mashreq/transfercoreservice/client/dto/TransactionChargesDto.java
@@ -6,16 +6,11 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@ApiModel(description = "for transaction charges")
 @Getter
 @Setter
 public class TransactionChargesDto {
-	@ApiModelProperty(notes = "the account class / type in which the charge is applicable")
 	private String accountClass;
-	@ApiModelProperty(notes = "transaction fee for local transactions")
 	private Double localTransactionCharge;
-	@ApiModelProperty(notes = "transaction fee for international transactions")
 	private Double internationalTransactionalCharge;
-	@ApiModelProperty(notes = "transaction fee from other banks if the transaction goes through other banks for fulfilment")
 	private Double coreBankTransactionFee;
 }
