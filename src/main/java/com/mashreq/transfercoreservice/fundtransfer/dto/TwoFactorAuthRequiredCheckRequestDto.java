8f3a7aa822576bb5beb5256ea69d491b5aa6b7f6
indrajithg <indrajithg@mashreq.com>
2022-04-18 17:13:53 +0530

    two factor authentication required validation api.

---
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/dto/TwoFactorAuthRequiredCheckRequestDto.java
@@ -0,0 +1,37 @@
+package com.mashreq.transfercoreservice.fundtransfer.dto;
+
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import com.mashreq.mobcommons.annotations.Account;
+import com.mashreq.transfercoreservice.annotations.TransactionAmount;
+
+import lombok.Data;
+
+@Data
+public class TwoFactorAuthRequiredCheckRequestDto {
+	@NotBlank
+	private String beneficiaryId;
+	
+	@TransactionAmount
+    @NotNull(message = "amount cannot be empty")
+    private BigDecimal amount;
+    
+    @Pattern(regexp = "^[a-zA-Z]{3}$")
+    @NotNull(message = "account currency cannot be empty")
+    private String accountCurrency;
+    
+    @Pattern(regexp = "^[a-zA-Z]{3}$")
+    @NotBlank(message = "txnCurrency cannot be empty")
+    private String txnCurrency;
+    
+    @Account
+    @NotBlank(message = "account number should be present")
+    private String fromAccount;
+    
+    @Pattern(regexp = "^$|[a-zA-Z0-9-]+",message="Not a valid Deal number")
+    private String dealNumber;
+}
--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/limits/DigitalUserLimitUsageRepository.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/limits/DigitalUserLimitUsageRepository.java
@@ -1,10 +1,16 @@
 package com.mashreq.transfercoreservice.fundtransfer.limits;
 
+import java.time.Instant;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DigitalUserLimitUsageRepository extends JpaRepository<DigitalUserLimitUsage, Long> {
+
+	@Query("select count(id) from DigitalUserLimitUsage usg where usg.beneficiaryId=:beneficiaryId and usg.createdDate between :fromDate and :toDate")
+	Long findCountForBeneficiaryIdBetweendates(Long beneficiaryId, Instant fromDate, Instant toDate);
 
 
 }
