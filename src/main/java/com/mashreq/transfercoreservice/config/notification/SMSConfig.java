6077c099bbecddaf60298c7b2970c2c4637a1c80
Nawazk@maskreq.com <nawazmd230@gmail.com>
2022-06-07 10:48:00 +0400

    masking the account number

--- src/main/java/com/mashreq/transfercoreservice/config/notification/SMSConfig.java
+++ src/main/java/com/mashreq/transfercoreservice/config/notification/SMSConfig.java
@@ -1,18 +1,22 @@
 package com.mashreq.transfercoreservice.config.notification;
 
 import com.mashreq.transfercoreservice.notification.model.CustomerNotification;
+import com.mashreq.transfercoreservice.notification.service.EmailUtil;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
 
-import static com.mashreq.transfercoreservice.notification.model.NotificationType.*;
-
 @Component
 @ConfigurationProperties(prefix = "app.notification.sms")
 @Data
 public class SMSConfig {
+
+    @Autowired
+    private EmailUtil emailUtil;
+
     private String priority;
     private String serviceId;
     private String callCenterNo;
@@ -22,11 +26,10 @@ public class SMSConfig {
     private String neoServiceId;
 
     public String getSMSTemplate(String type, CustomerNotification customerNotification) {
-        if(type.contains("PL") && type.contains("CREATION")){
-            return MessageFormat.format(plSiCreation, customerNotification.getBeneficiaryName(), customerNotification.getCreditAccount(),customerNotification.getSegment().getCustomerCareNumber());
-        }
-        else {
-            return MessageFormat.format(ownAccountTransactionInitiated, customerNotification.getCurrency(),customerNotification.getAmount(),customerNotification.getTxnRef(),customerNotification.getSegment().getCustomerCareNumber());
+        if (type.contains("PL") && type.contains("CREATION")) {
+            return MessageFormat.format(plSiCreation, customerNotification.getBeneficiaryName(), emailUtil.doMask(customerNotification.getCreditAccount()), customerNotification.getSegment().getCustomerCareNumber());
+        } else {
+            return MessageFormat.format(ownAccountTransactionInitiated, customerNotification.getCurrency(), customerNotification.getAmount(), customerNotification.getTxnRef(), customerNotification.getSegment().getCustomerCareNumber());
         }
     }
 }
