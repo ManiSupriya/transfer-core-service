1bb62eac31dd53f54cd992818ab16be9ab3d51aa
Hareesh Ponnappan <hareeshp@mashreq.com>
2022-08-01 18:54:45 +0400

    85536 - otp releaxation changes

--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/repository/TransferLimitRepository.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/repository/TransferLimitRepository.java
@@ -1,5 +1,6 @@
 package com.mashreq.transfercoreservice.fundtransfer.repository;
 
+import com.mashreq.transfercoreservice.model.TransferDetails;
 import com.mashreq.transfercoreservice.model.TransferLimit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,9 +10,6 @@ import java.time.Instant;
 
 @Repository
 public interface TransferLimitRepository extends JpaRepository<TransferLimit, Long> {
-    @Query("SELECT count(id) as id, SUM(paid_amount) as amount from TransactionLimit limit where limit" +
-            ".beneficiaryId=:beneficiaryId and" +
-            " limit.createdDate between :fromDate and :toDate")
-    TransferLimit findTransactionCountAndTotalAmountBetweenDates(Long beneficiaryId, Instant fromDate,
-                                                                 Instant toDate);
+    @Query(nativeQuery = true)
+    TransferDetails findTransactionCountAndTotalAmountBetweenDates(Long beneficiaryId, Instant fromDate, Instant toDate);
 }
