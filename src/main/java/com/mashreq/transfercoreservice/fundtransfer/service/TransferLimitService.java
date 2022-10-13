ab03e56876511465da898ffc9741ee6b8f274ef6
Krishna Kondru <krishnako@mashreq.com>
2022-10-13 12:16:34 +0400

    Currency conversion to AED

--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/service/TransferLimitService.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/service/TransferLimitService.java
@@ -1,5 +1,9 @@
 package com.mashreq.transfercoreservice.fundtransfer.service;
 
+import com.mashreq.transfercoreservice.client.dto.AccountDetailsDTO;
+import com.mashreq.transfercoreservice.client.dto.CoreCurrencyConversionRequestDto;
+import com.mashreq.transfercoreservice.client.dto.CurrencyConversionDto;
+import com.mashreq.transfercoreservice.client.service.MaintenanceService;
 import com.mashreq.transfercoreservice.fundtransfer.dto.TransferLimitRequestDto;
 import com.mashreq.transfercoreservice.fundtransfer.dto.TransferLimitResponseDto;
 import com.mashreq.transfercoreservice.fundtransfer.repository.TransferLimitRepository;
@@ -9,12 +13,15 @@ import org.springframework.stereotype.Service;
 
 import static com.mashreq.transfercoreservice.common.HtmlEscapeCache.htmlEscape;
 
+import java.math.BigDecimal;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TransferLimitService {
 
     private final TransferLimitRepository repository;
+    private final MaintenanceService maintenanceService;
 
     public TransferLimitResponseDto validateAndSaveTransferDetails(TransferLimitRequestDto limitRequestDto,
                                                                    String transactionRefNo) {
@@ -38,12 +45,30 @@ public class TransferLimitService {
                 .build();
     }
 
-    public TransferLimitResponseDto saveTransferDetails(TransferLimitRequestDto limitDto, String transactionRefNo) {
-        log.info("Storing transferred/configured amount {} for beneficiary {}", htmlEscape(limitDto.getAmount()),
-                htmlEscape(limitDto.getBeneficiaryId()));
-        repository.save(limitDto.toEntity(transactionRefNo));
-        return TransferLimitResponseDto.builder()
-                .success(true)
-                .build();
-    }
+
+	public TransferLimitResponseDto saveTransferDetails(TransferLimitRequestDto limitDto, String transactionRefNo) {
+		log.info("Storing transferred/configured amount {} for beneficiary {}", htmlEscape(limitDto.getAmount()),
+				htmlEscape(limitDto.getBeneficiaryId()));
+		limitDto.setAmount(getAmountBasedOnCurrency(limitDto));
+		log.info("Storing coverted amount {} for beneficiary {}", htmlEscape(limitDto.getAmount()),
+				htmlEscape(limitDto.getBeneficiaryId()));
+		repository.save(limitDto.toEntity(transactionRefNo));
+		return TransferLimitResponseDto.builder().success(true).build();
+	}
+
+	private BigDecimal getAmountBasedOnCurrency(final TransferLimitRequestDto limitDto) {
+		return "AED".equalsIgnoreCase(limitDto.getAccountCurrency()) ? limitDto.getAmount()
+				: convertAmountInLocalCurrency(limitDto);
+	}
+
+	private BigDecimal convertAmountInLocalCurrency(final TransferLimitRequestDto limitDto) {
+		CoreCurrencyConversionRequestDto currencyConversionRequestDto = new CoreCurrencyConversionRequestDto();
+		currencyConversionRequestDto.setAccountNumber(limitDto.getAccountNumber());
+		currencyConversionRequestDto.setAccountCurrency(limitDto.getAccountCurrency());
+		currencyConversionRequestDto.setAccountCurrencyAmount(limitDto.getAmount());
+		currencyConversionRequestDto.setTransactionCurrency("AED");
+
+		CurrencyConversionDto currencyConversionDto = maintenanceService.convertCurrency(currencyConversionRequestDto);
+		return currencyConversionDto.getTransactionAmount();
+	}
 }
