4b4d0386cb4e263926435c9a8083065e98bf9f0f
Nawazk@maskreq.com <nawazmd230@gmail.com>
2022-11-14 10:29:30 +0400

    adding one Table conncection

--- src/main/java/com/mashreq/transfercoreservice/api/FundTransferController.java
+++ src/main/java/com/mashreq/transfercoreservice/api/FundTransferController.java
@@ -22,6 +22,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
