5e63cb0e05030d6c9d47b35cf5a82789213f5040
Nawazk@maskreq.com <nawazmd230@gmail.com>
2022-11-14 08:51:25 +0400

    adding one Table conncection

---
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/dto/NpssEnrolmentRepo.java
@@ -0,0 +1,32 @@
+package com.mashreq.transfercoreservice.fundtransfer.dto;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="npss_enrollment")
+@Data
+@NoArgsConstructor
+public class NpssEnrolmentRepo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String cif_id;
+    @Column(nullable = false)
+    private String enrollment_status;
+    @Column(nullable = false)
+    private String created_date;
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+}
