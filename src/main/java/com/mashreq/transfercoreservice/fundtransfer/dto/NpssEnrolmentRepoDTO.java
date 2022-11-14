fd88c7f37b8cfbb13e70ded084c36bf49eb0f7a4
Nawazk@maskreq.com <nawazmd230@gmail.com>
2022-11-15 03:58:29 +0400

    Updating URL's

--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/dto/NpssEnrolmentRepoDTO.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/dto/NpssEnrolmentRepoDTO.java
@@ -1,7 +1,9 @@
 package com.mashreq.transfercoreservice.fundtransfer.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,11 +11,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.time.Instant;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="npss_enrollment")
 public class NpssEnrolmentRepoDTO {
     @Id
