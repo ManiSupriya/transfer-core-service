ef8c6cff5dcc38cd2d0b8e886e94f7fce7cb04e8
Krishna Kondru <krishnako@mashreq.com>
2023-12-07 10:15:34 +0400

    Account class Addition for Fund transfer changes

--- src/main/java/com/mashreq/transfercoreservice/fundtransfer/service/FundTransferMWService.java
+++ src/main/java/com/mashreq/transfercoreservice/fundtransfer/service/FundTransferMWService.java
@@ -280,10 +280,7 @@ public class FundTransferMWService {
         } else {
         	fundTransferReqType.setDealFlag(YesNo.N.name());
         }
-
-        if(LOCAL.equalsIgnoreCase(request.getServiceType())){
             fundTransferReqType.setAccountClass(request.getAccountClass());
-        }
         services.getBody().setFundTransferReq(fundTransferReqType);
         log.info("EAI Service request for fund transfer prepared {}", htmlEscape(services));
         return services;
