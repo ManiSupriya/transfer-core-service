460a602a65c5ccf7cdf14423679e64a82cbcbb4c
Krishna Kondru <krishnako@mashreq.com>
2024-01-24 16:04:19 +0400

    Loggers added

--- src/main/java/com/mashreq/transfercoreservice/banksearch/UAEAccountTitleFetchService.java
+++ src/main/java/com/mashreq/transfercoreservice/banksearch/UAEAccountTitleFetchService.java
@@ -38,7 +38,7 @@ public class UAEAccountTitleFetchService {
     }
 
     private String fetchTitleFromResponse(UaeIbanTitleFetchResponse uaeIbanTitleFetchResponse){
-        log.info("Response received {}",uaeIbanTitleFetchResponse.toString());
+        log.info("Response received {}", uaeIbanTitleFetchResponse);
         return Optional.ofNullable(uaeIbanTitleFetchResponse)
                 .map(UaeIbanTitleFetchResponse::getUaeIbanTitleFetch)
                 .map(UaeIbanTitleFetchDto::getUaeIbanTitleFetchDtoList)
