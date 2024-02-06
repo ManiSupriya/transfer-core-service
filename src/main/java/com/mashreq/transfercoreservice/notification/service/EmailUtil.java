33ee4a976c6b978ba31c9889bc5572143eb30ee4
ChippeS <chippeS@mashre.com>
2024-02-06 09:47:57 +0400

    128790: Code updated as per PR review

--- src/main/java/com/mashreq/transfercoreservice/notification/service/EmailUtil.java
+++ src/main/java/com/mashreq/transfercoreservice/notification/service/EmailUtil.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashreq.ms.exceptions.GenericExceptionHandler;
 import com.mashreq.transfercoreservice.errors.TransferErrorCode;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
